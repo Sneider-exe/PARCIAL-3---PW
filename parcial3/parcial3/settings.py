b8e715c902a7d43dc840cd7e1c87fbb18ac53e4d
Sneider-exe <brayan2220083@correo.uis.edu.co>
2023-11-29 23:46:52 -0500

    Arreglo base y modelos

--- parcial3/parcial3/settings.py
+++ parcial3/parcial3/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notas.apps.NotasConfig',
     'commons.apps.CommonsConfig',
+    'usuario.apps.UsuarioConfig',
 ]
 
 MIDDLEWARE = [
